fc992d8efa950329bc5be6691ab95fae33695c8d
Paula Murphy <paulam849@gmail.com>
2015-12-28 22:45:28 -0500

    first commit

---
+++ db/migrate/20151229025143_create_users_table.rb
@@ -0,0 +1,11 @@
+class CreateUsersTable < ActiveRecord::Migration
+  def change
+  	create_table :users do |t|
+  		t.string :fname
+  		t.string :lname
+  		t.string :email
+  		t.string :password
+  		t.timestamps null: false
+  	end
+  end
+end
